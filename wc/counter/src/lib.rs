710c1904740784b8bbc27ef292d87acc76f91804
Andreas Krath <andreas.krath+github@gmail.com>
2023-07-12 01:03:36 +0200

    Add byte count functionality and multiple test cases for each functionality

--- wc/counter/src/lib.rs
+++ wc/counter/src/lib.rs
@@ -2,9 +2,7 @@
 ///
 /// Newlines are defined with the newline character `\n`.
 pub fn line_count(input: &str) -> usize {
-    // all input has at least one line,
-    // no matter if it contains a newline character
-    1 + input.matches('\n').count()
+    input.lines().count()
 }
 
 /// The `word_count` function returns the amount of words in the input string.
@@ -15,36 +13,57 @@ pub fn word_count(input: &str) -> usize {
     input.split_whitespace().count()
 }
 
+/// The `byte_count` function returns the amount of bytes in the input string.
+pub fn byte_count(input: &str) -> usize {
+    input.len()
+}
+
 #[cfg(test)]
 mod unit_tests {
-    use crate::{line_count, word_count};
+    use crate::{byte_count, line_count, word_count};
+    use rstest::rstest;
 
-    #[test]
-    fn lines() {
-        let input = "Hello, world!";
-        let expected = 1;
+    #[rstest]
+    #[case("Hello, world!", 1)]
+    #[case("Hello\n world!", 2)]
+    #[case("\nHello,\n world!", 3)]
+    #[case("Hello, world!\n\n\nGoodbye!", 4)]
+    #[case("\n", 1)]
+    fn lines(#[case] input: &str, #[case] expected: usize) {
         let actual = line_count(input);
         assert_eq!(actual, expected);
     }
 
-    #[test]
-    fn words() {
-        let input = "Hello, world! How are you feeling today?";
-        let expected = 7;
+    #[rstest]
+    #[case("", 0)]
+    #[case(" ", 0)]
+    #[case("Hello world!", 2)]
+    #[case("Hello, world!\nGoodbye!", 3)]
+    fn words(#[case] input: &str, #[case] expected: usize) {
         let actual = word_count(input);
+        assert_eq!(actual, expected);
+    }
+
+    #[rstest]
+    #[case("", 0)]
+    #[case("Hello, world! How are you feeling today?", 40)]
+    #[case(" ", 1)]
+    #[case("\n\n\n", 3)]
+    fn bytes(#[case] input: &str, #[case] expected: usize) {
+        let actual = byte_count(input);
         assert_eq!(actual, expected);
     }
 }
 
 #[cfg(test)]
 mod file_tests {
-    use crate::{line_count, word_count};
+    use crate::{byte_count, line_count, word_count};
     use std::fs::read_to_string;
 
     #[test]
     fn words() {
         let input = read_to_string("test.txt").expect("should find test file");
-        let expected = 58159;
+        let expected = 58159; // what `wc -w test.txt` produces locally
         let actual = word_count(input.as_str());
         assert_eq!(actual, expected);
     }
@@ -52,8 +71,16 @@ mod file_tests {
     #[test]
     fn lines() {
         let input = read_to_string("test.txt").expect("should find test file");
-        let expected = 7137;
+        let expected = 7136; // what `wc -l test.txt` produces locally
         let actual = line_count(input.as_str());
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn bytes() {
+        let input = read_to_string("test.txt").expect("should find test file");
+        let expected = 334695; // what `wc -c test.txt` produces locally
+        let actual = byte_count(input.as_str());
         assert_eq!(actual, expected);
     }
 }
