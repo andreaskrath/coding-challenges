dfe93addecfbb055d70782876f665f05f96e5374
Andreas Krath <70445343+andreaskrath@users.noreply.github.com>
2023-07-12 22:33:43 +0200

    Delete uniq directory

--- uniq/main/src/main.rs
+++
@@ -1,8 +0,0 @@
-use lib::lines::Uniq;
-
-fn main() {
-    let input = std::fs::read_to_string("countries.txt").expect("failed to read input file");
-    let unique_finder = Uniq::new(&input);
-    let res = unique_finder.get_uniq_lines();
-    println!("{:#?}\n\nLength = {}", res, res.len());
-}
